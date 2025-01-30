deea9f1b10e5f954c0c7c76aa8080f83a38c94a4
jkryszto00 <jakubkryszto@gmail.com>
2025-01-30 12:20:37 +0100

    vite https fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    base: import.meta.env.VITE_APP_URL,
     plugins: [
         vue(),
         laravel({
